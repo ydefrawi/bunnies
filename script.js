f10edda2b910c6d3d46b7e805d10fc3712b4b6c9
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-07 21:49:33 -0500

    got rabbits to pair up. random male selected, female array sorts and male chooses female with highest wildness

--- script.js
+++ script.js
@@ -6,7 +6,13 @@
 // Perhaps a gestation period where said person could not procreate during measured in iterations.
 // Progeny shouldn't be eligible for procreation for a span of iterations aka maturation.
 //sit back watch your creations blink in and out of existence
-
+const asciiHeart=`
+,d88b.d88b,
+88888888888
+'Y8888888Y'
+  'Y888Y'   
+    'Y'
+`
 const rabbitNames = require('./rabbitNames')
 const { Rabbit, Male, Female } = require('./rabbitClasses')
 
@@ -32,7 +38,7 @@ const getRandomInt = (min, max) => {
 //These Are the first rabbits randomly spawned
 const startingGroup = () => {
     randBool = randBoolean();
-    const rand2to5 = getRandomInt(2, 5)
+    const rand2to5 = getRandomInt(2, 10)
     const x = rand2to5;
     console.log(`You start with ${x} rabbits\n------------------`)
     for (let index = 0; index < x; index++) {
@@ -52,18 +58,45 @@ const startingGroup = () => {
 
 selectMale = () =>{
     randomMale=maleRabbits[getRandomInt(0, maleRabbits.length)]
-    console.log(randomMale)
+    // console.log(`\n--------------`)
+    // console.log(`ALPHA MALE`)
+    // console.log(randomMale)
+    // console.log(`---------------\n`)
     return randomMale;
 }
 
 selectFemale = () =>{
-    let wildnessArray = femaleRabbits.map(a=>a.wildness);
-    console.log(wildnessArray)
+    femaleRabbits.sort((a,b) => {
+        return b.wildness - a.wildness;
+      })
+    alphaFemale=femaleRabbits[0];
+    return alphaFemale
+    // let wildnessArray = femaleRabbits.map(a=>a.wildness);
+    // console.log(wildnessArray)
 }
 
 letsMingle = () =>{
-    randomMale = selectMale();
+    male = selectMale();
+    female = selectFemale()
+    console.log(`\n${male.name} has decided to take the lead\n`)
+    setTimeout(() => {
+        console.log(`${male.name} is looking for a wild female.\n`)
+    }, 2000);
     
+    setTimeout(() => {
+        console.log(`Who's that special someone?\n`)
+    }, 2500);
+    setTimeout(() => {
+        console.log(`...\n`)
+    }, 3000);
+    setTimeout(() => {
+        console.log(`${male.name} has chosen ${female.name}!`)
+    }, 4000);
+    setTimeout(() => {
+        console.log(asciiHeart)
+    }, 5000);
+
+
 
 }
 
@@ -79,9 +112,7 @@ letsMingle = () =>{
 
 init = () => {
     startingGroup()
-    selectMale()
-    console.log(femaleRabbits)
-    selectFemale()
+    letsMingle()
     // makeRabbits()
 }
 
